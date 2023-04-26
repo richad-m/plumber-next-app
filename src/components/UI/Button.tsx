f26501bc5256d2737662ee9c4606bcd37b68a18b
richad <richad.meralli@gmail.com>
2023-04-26 23:05:27 +0200

    feat: button should act as link to tel

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 
-function Button({ text, styleType }: { text: string; styleType: string }) {
+function Button({
+  text,
+  styleType,
+  as,
+}: {
+  text: string;
+  styleType: string;
+  as?: string;
+}) {
+  if (as === "a") {
+    return (
+      <a href="tel:0100000000" className={`${styleType} button-as-link`}>
+        {text}
+      </a>
+    );
+  }
   return <button className={styleType}>{text}</button>;
 }
 
