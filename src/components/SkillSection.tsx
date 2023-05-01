f257bc7a10ae732460b4d1abf58169cec4594c52
richad <richad.meralli@gmail.com>
2023-05-01 15:24:59 +0200

    feat: add mobile menu navigation

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -5,7 +5,7 @@ import Separator from "./UI/Separator";
 function SkillSection() {
   return (
     <>
-      <section className="skill-section" id="skill-section">
+      <section className="skill-section" id="service">
         <h2 className="skill-title text-4xl teal">Services</h2>
         <div className="block sm:flex skill-container">
           <Card />
