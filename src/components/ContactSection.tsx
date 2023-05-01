a5892975f8e2377b76bb7e9a4535117f1bfcc389
richad <richad.meralli@gmail.com>
2023-05-01 18:07:04 +0200

    feat: add style to contact form

---
+++ src/components/ContactSection.tsx
@@ -0,0 +1,11 @@
+import ContactForm from "./ContactForm";
+
+function ContactSection() {
+  return (
+    <section className="skill-section" id="contact">
+      <ContactForm />
+    </section>
+  );
+}
+
+export default ContactSection;
