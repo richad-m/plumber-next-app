5cfc3057b0e42cc2df3fbc5cd659e0c258115f1f
richad <richad.meralli@gmail.com>
2023-05-14 13:38:41 +0200

    feat: edit banner image

--- src/constants/imageUrl.ts
+++ src/constants/imageUrl.ts
@@ -3,6 +3,6 @@ export const BANNER_SLIDES_URL = [
     url: "https://images.unsplash.com/photo-1620626011761-996317b8d101?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80",
   },
   {
-    url: "  https://images.unsplash.com/photo-1530124566582-a618bc2615dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    url: "https://images.unsplash.com/photo-1581244277943-fe4a9c777189?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80",
   },
 ];
