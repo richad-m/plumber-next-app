10c230b38143c318c800a4e9f866fe27e2a132a0
richad <richad.meralli@gmail.com>
2023-05-17 13:16:07 +0200

    feat :edit photo

--- src/constants/imageUrl.ts
+++ src/constants/imageUrl.ts
@@ -1,6 +1,6 @@
 export const BANNER_SLIDES_URL = [
   {
-    url: "https://images.unsplash.com/photo-1620626011761-996317b8d101?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80",
+    url: "https://images.unsplash.com/photo-1495774539583-885e02cca8c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   },
   {
     url: "https://images.unsplash.com/photo-1581244277943-fe4a9c777189?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80",
