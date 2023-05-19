de07b059fd1157757d4b8722ebea3a1c558e6562
richad <richad.meralli@gmail.com>
2023-05-19 08:43:35 +0200

    feat: use new component section in partnershipSection

--- src/components/PartnershipSection.tsx
+++ src/components/PartnershipSection.tsx
@@ -1,27 +1,23 @@
 import React from "react";
 import BrandCarrousel from "./BrandCarrousel";
+import Section from "./UI/Section";
 import Separator from "./UI/Separator";
 
 function PartnershipSection() {
   return (
-    <>
-      <section id="pricing">
-        <h2>Nos partenaires de marque</h2>
-        <br />
-        <div className="">
-          Chez AC Pro, nous sommes conscients de l&apos;impact que peut avoir
-          une plomberie défaillante sur le fonctionnement de votre appartement
-          ou maison. C&apos;est pourquoi nous nous engageons à vous offrir des
-          services haut de gamme, professionnels et abordables, pour que votre
-          plomberie fonctionne toujours à son meilleur niveau. Que vous ayez
-          besoin d&apos;une intervention en urgence ou pour toute autre besoin,
-          notre équipe de professionnels est à votre disposition pour vous aider
-          à résoudre tous vos problèmes de plomberie de manière rapide et
-          efficace.
-        </div>
-        <BrandCarrousel />
-      </section>
-    </>
+    <Section id="partenaires" title="Nos partenaires de marque">
+      <p>
+        Chez AC Pro, nous sommes conscients de l&apos;impact que peut avoir une
+        plomberie défaillante sur le fonctionnement de votre appartement ou
+        maison. C&apos;est pourquoi nous nous engageons à vous offrir des
+        services haut de gamme, professionnels et abordables, pour que votre
+        plomberie fonctionne toujours à son meilleur niveau. Que vous ayez
+        besoin d&apos;une intervention en urgence ou pour toute autre besoin,
+        notre équipe de professionnels est à votre disposition pour vous aider à
+        résoudre tous vos problèmes de plomberie de manière rapide et efficace.
+      </p>
+      <BrandCarrousel />
+    </Section>
   );
 }
 
