8325cd3b1d08d3298d445d22f183075faf05002e
richad <richad.meralli@gmail.com>
2023-08-03 18:59:11 +0200

    feat: edit background

--- src/constants/imageUrl.ts
+++ src/constants/imageUrl.ts
@@ -1,5 +1,5 @@
 export const BANNER_SLIDES_URL = [
   {
-    url: "https://images.unsplash.com/photo-1581244277943-fe4a9c777189?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80",
+    url: "https://images.unsplash.com/photo-1530124566582-a618bc2615dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
   },
 ];
