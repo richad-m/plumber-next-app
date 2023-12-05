f1faf20a17ce2b44390a4dd78bd1f1196cd0ff04
richad <richad.meralli@gmail.com>
2023-12-05 18:14:08 +0100

    build: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         icon={true}
         closeButton={true}
       />
+      <Analytics />
     </>
   );
 }
